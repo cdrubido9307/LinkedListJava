adee5fe21d994a1b177caa825e0eb1ea3146ea1d
Carlos Rubido <cdrubido@live.unc.edu>
2021-03-04 22:41:29 -0500

    Started working in LinkedList Implementation

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,23 @@
+package linkedlist;
+
+public class LinkedList<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public LinkedList () {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.size ==0;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public 
+}
