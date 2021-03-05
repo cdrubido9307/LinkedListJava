e4ab87b4843c17551998cd196699aa2378429c9b
Guillermo Saez <guisaez@ad.emaill.unc.edu>
2021-03-05 00:10:15 -0500

    LinkedList (methods)

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -19,7 +19,7 @@ public class LinkedList<E> {
         return this.size;
     }
 
-    public void insert(E element) {
+    public void add(E element) {
         Node newNode = new NodeImpl(element, null, null);
         if (isEmpty()) {
             head = newNode;
@@ -33,6 +33,76 @@ public class LinkedList<E> {
         }
     }
 
+    public void insert(int index, E element) {
+        if (index > size) {
+            validateIndex(index);
+        }
+        Node current = head;
+        int i = 0;
+        if (index == 0) {
+            if (isEmpty()) {
+                add(element);
+                return;
+            } else {
+                Node newNode = new NodeImpl(element, head, null);
+                head.setPrevious(newNode);
+                head = newNode;
+                size++;
+                return;
+            }
+        } else if (index == size) {
+            add(element);
+            return;
+        }
+
+        while (current != null) {
+            if (i == (index - 1)) {
+                Node temp = current.getNext();
+                Node newNode = new NodeImpl(element, temp, current);
+                current.getNext().setPrevious(newNode);
+                current.setNext(newNode);
+                size++;
+                return;
+            } else {
+                current = current.getNext();
+                i++;
+            }
+
+        }
+    }
+
+    public int indexOf(E element) {
+        Node current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.getValue() == element) {
+                return index;
+            }
+            index++;
+            current = current.getNext();
+        }
+
+        return -1;
+    }
+
+    public E getValue(int index) {
+        validateIndex(index);
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return (E) current.getValue();
+    }
+
+
+
+
+    public void validateIndex(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+    }
+
     public String toString() {
         String list = "";
         Node current = head;
@@ -40,7 +110,7 @@ public class LinkedList<E> {
             if(current.getNext() == null)
                 list+= current.getValue();
             else
-                list += current.getValue() + " -> ";
+                list += current.getValue() + " <--> ";
             current = current.getNext();
         }
         return list;
--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -3,16 +3,31 @@ package linkedlist;
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list1 = new LinkedList<Integer>();
-        list1.insert(2);
-        list1.insert(3);
-        list1.insert(5);
-        list1.insert(8);
+        list1.add(2);
+        list1.add(3);
+        list1.add(5);
+
+        list1.insert(3, 1);
+        list1.insert(0,10);
         System.out.println(list1.toString());
 
+        list1.insert(5,20);
+        list1.insert(4,30);
+        System.out.println(list1.toString());
+
+        list1.insert(3,55);
+        list1.insert(6,66);
+        list1.add(8);
+        System.out.println(list1.toString());
+
+        System.out.println(list1.getValue(1));
+        System.out.println(list1.getValue(9));
+        System.out.println(list1.getValue(4));
+
         LinkedList<String> list2 = new LinkedList<String>();
-        list2.insert("Laritza");
-        list2.insert("Maikel el 22");
-        list2.insert("Africa");
+        list2.add("Laritza");
+        list2.add("Maikel el 22");
+        list2.add("Africa");
         System.out.println(list2.toString());
 
 
